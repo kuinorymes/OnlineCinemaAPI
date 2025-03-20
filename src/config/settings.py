75b88dd4ac8a3ed3aa319d0609adfba9ad578d12
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-21 00:43:31 +0200

    - Add .env.sample for DB_URL - Installed dotenv

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,11 +1,14 @@
+import os
 from pathlib import Path
 
 from pydantic.v1 import BaseSettings
 
+from dotenv import load_dotenv
+load_dotenv()
 
 class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
-    PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "theater.db")
+    PATH_TO_DB: str = os.getenv("DATABASE_URL", str(BASE_DIR / "database" / "source" / "theater.db"))
     PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "database" / "seed_data" / "imdb_movies.csv")
 
 
