a54e6d0d553cf68f857da28e0177443e4477f7d5
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-22 11:48:38 +0200

    reformatted code via black

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,18 +4,23 @@ from pathlib import Path
 from pydantic.v1 import BaseSettings
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
-    PATH_TO_DB: str = os.getenv("DATABASE_URL", str(BASE_DIR / "database" / "source" / "theater.db"))
-    PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "database" / "seed_data" / "imdb_movies.csv")
+    PATH_TO_DB: str = os.getenv(
+        "DATABASE_URL", str(BASE_DIR / "database" / "source" / "theater.db")
+    )
+    PATH_TO_MOVIES_CSV: str = str(
+        BASE_DIR / "database" / "seed_data" / "imdb_movies.csv"
+    )
 
 
 class Settings(BaseAppSettings):
-    ...
+    pass
 
 
 class TestingSettings(BaseAppSettings):
-    ...
+    pass
