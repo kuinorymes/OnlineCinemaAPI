9297ac3121d681317868152dc7930a9ef4258451
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-23 21:09:51 +0200

    Implemented minor changes to imports to apply migrations

--- src/database/models/payments.py
+++ src/database/models/payments.py
@@ -6,7 +6,9 @@ from sqlalchemy import Integer, DateTime, String, DECIMAL, ForeignKey, func
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import Enum as SQLAlchemyEnum
 
-from database.models.base import Base
+from src.database.models.users import UserModel
+from src.database.models.orders import OrderModel, OrderItemModel
+from src.database.models.base import Base
 
 
 class PaymentStatusEnum(str, Enum):
