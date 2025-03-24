fa59412c39c96921ec3b8e128b8f79aff4d95db2
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-03-24 14:13:27 +0200

    update BaseAppSettings class
    # Conflicts:
    #	src/config/settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -33,14 +33,6 @@ class BaseAppSettings(BaseSettings):
 
     API_VERSION: str = "/api/v1"
 
-    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
-    CELERY_BACKEND_URL: str = "redis://localhost:6379/0"
-
-    S3_ACCESS_KEY: str = os.getenv("S3_ACCESS_KEY")
-    S3_SECRET_KEY: str = os.getenv("S3_SECRET_KEY")
-    S3_ENDPOINT_URL: str = os.getenv("S3_ENDPOINT_URL")
-    S3_BUCKET_NAME: str = os.getenv("S3_BUCKET_NAME")
-
     STRIPE_WEBHOOK_SECRET: str = os.getenv("STRIPE_WEBHOOK_SECRET")
     STRIPE_SECRET_KEY: str = os.getenv("STRIPE_SECRET_KEY")
 
