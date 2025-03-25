6e55f68c33b018f15d83af81f298e32d8c9cb814
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-25 16:41:52 +0200

    Reformatted code: Black

--- src/security/permissions.py
+++ src/security/permissions.py
@@ -3,7 +3,7 @@ from fastapi import Depends, HTTPException, status
 from routes.users import get_current_user
 
 
-def is_admin(current_user = Depends(get_current_user)):
+def is_admin(current_user=Depends(get_current_user)):
     if current_user.group.name != "ADMIN":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Access denied. Admins only."
