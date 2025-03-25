c35547fcf910d4334fa62ecbc1ce7889f5a1c097
10kkyvl <ztkkre@gmail.com>
2025-03-25 18:04:43 +0200

    added movie router

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from routes.users import router as users_router
 from routes.orders import router as orders_router
 from routes.payments import router as payments_router
+from routes import movies_router
 from routes.cart import router as cart_router
+
 from config.dependencies import get_settings
 
 
@@ -28,6 +30,7 @@ app.include_router(users_router, tags=["users"], prefix=f"{api_version_prefix}/u
 app.include_router(
     orders_router, tags=["orders"], prefix=f"{api_version_prefix}/orders"
 )
+app.include_router(movies_router, tags=["movies"], prefix=f"{api_version_prefix}/theater")
 app.include_router(
     payments_router, tags=["payments"], prefix=f"{api_version_prefix}/payments"
 )
