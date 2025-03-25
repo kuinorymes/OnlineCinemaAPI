64e90d29ed05b24bfdde7203fb93f0f1e1c1260b
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-25 16:41:22 +0200

    Removed is_admin function from dependencies due to circular import - Added it to security/permissions

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,8 +1,6 @@
 import os
 
-from fastapi import Depends, HTTPException, status
-from src.routes.auth import get_current_user
-from src.database.models import User
+from fastapi import Depends
 from config.settings import BaseAppSettings, TestingSettings, Settings
 from security.interfaces import JWTAuthManagerInterface
 from security.jwt_manager import JWTAuthManager
@@ -40,14 +38,6 @@ def get_jwt_auth_manager(
         secret_key_refresh=settings.SECRET_REFRESH_TOKEN_KEY,
         algorithm=settings.JWT_ALGORITHM,
     )
-
-
-def is_admin(current_user: User = Depends(get_current_user)) -> User:
-    if current_user.group.name != "ADMIN":
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Access denied. Admins only."
-        )
-    return current_user
   
 def get_s3_storage_client(
     settings: BaseAppSettings = Depends(get_settings),
