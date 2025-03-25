a7d1e3b140e4535cd0d34b86ee3087e655335245
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-25 11:35:46 +0200

    Add get_s3_storage_cliend dependency

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -5,6 +5,8 @@ from fastapi import Depends
 from config.settings import BaseAppSettings, TestingSettings, Settings
 from security.interfaces import JWTAuthManagerInterface
 from security.jwt_manager import JWTAuthManager
+from storages.interfaces import S3StorageInterface
+from storages.s3 import S3StorageClient
 
 
 def get_settings() -> BaseAppSettings:
@@ -36,4 +38,29 @@ def get_jwt_auth_manager(
         secret_key_access=settings.SECRET_ACCESS_TOKEN_KEY,
         secret_key_refresh=settings.SECRET_REFRESH_TOKEN_KEY,
         algorithm=settings.JWT_ALGORITHM,
+    )
+
+
+def get_s3_storage_client(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> S3StorageInterface:
+    """
+    Retrieve an instance of the S3StorageInterface configured with the application settings.
+
+    This function instantiates an S3StorageClient using the provided settings, which include the S3 endpoint URL,
+    access credentials, and the bucket name. The returned client can be used to interact with an S3-compatible
+    storage service for file uploads and URL generation.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings,
+        provided via dependency injection from `get_settings`.
+
+    Returns:
+        S3StorageInterface: An instance of S3StorageClient configured with the appropriate S3 storage settings.
+    """
+    return S3StorageClient(
+        endpoint_url=settings.S3_STORAGE_ENDPOINT,
+        access_key=settings.S3_STORAGE_ACCESS_KEY,
+        secret_key=settings.S3_STORAGE_SECRET_KEY,
+        bucket_name=settings.S3_BUCKET_NAME
     )
