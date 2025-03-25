34f1f32540a6c31e8e91f1ca24c2b3129e15f937
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-25 14:39:02 +0200

    Add lazy=selectin to order_items relations

--- src/database/models/orders.py
+++ src/database/models/orders.py
@@ -30,9 +30,11 @@ class OrderModel(Base):
         Numeric(10, 2), nullable=True
     )
 
-    user: Mapped["UserModel"] = relationship(back_populates="orders")  # noqa: F821
+    user: Mapped["UserModel"] = relationship("UserModel")  # noqa: F821
     order_items: Mapped[List["OrderItemModel"]] = relationship(
-        back_populates="order", cascade="all, delete"
+        back_populates="order",
+        lazy="selectin",
+        cascade="all, delete"
     )
     payments: Mapped[List["PaymentModel"]] = relationship(  # noqa: F821
         "PaymentModel", back_populates="order"
