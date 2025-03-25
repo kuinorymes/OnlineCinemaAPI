b5fa58e73f3ade5e163f979fb933c67c87f9af45
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-25 13:16:10 +0200

    edited cart asyncio edited imports fixed flake8

--- src/routes/cart.py
+++ src/routes/cart.py
@@ -1,22 +1,24 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from sqlalchemy.orm import Session
-from src.database import SessionLocal, engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from src.database.models import models
 from src.config.dependencies import is_admin
-from src.routes.users import get_current_user, get_db
+from src.database.session_sqlite import get_sqlite_db
+from src.routes.users import get_current_user
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
 @app.get("/admin/users/{user_id}/cart", status_code=status.HTTP_200_OK)
-def admin_view_user_cart(
+async def admin_view_user_cart(
     user_id: int,
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_sqlite_db),
     current_admin: models.User = Depends(is_admin),
-):
-
-    cart = db.query(models.Cart).filter(models.Cart.user_id == user_id).first()
+) -> dict:
+    result = await db.execute(
+        select(models.Cart).filter(models.Cart.user_id == user_id)
+    )
+    cart = result.scalars().first()
     if not cart:
         return {"items": []}
 
@@ -35,67 +37,78 @@ def admin_view_user_cart(
 
 
 @app.post("/cart/items", status_code=status.HTTP_201_CREATED)
-def add_item_to_cart(
+async def add_item_to_cart(
     movie_id: int,
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_sqlite_db),
     current_user: models.User = Depends(get_current_user),
-) -> models.Item:
-    movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+):
+    result = await db.execute(select(models.Movie).filter(models.Movie.id == movie_id))
+    movie = result.scalars().first()
     if not movie:
         raise HTTPException(status_code=404, detail="Movie not found.")
 
-    cart = db.query(models.Cart).filter(models.Cart.user_id == current_user.id).first()
+    result = await db.execute(
+        select(models.Cart).filter(models.Cart.user_id == current_user.id)
+    )
+    cart = result.scalars().first()
     if not cart:
         cart = models.Cart(user_id=current_user.id)
         db.add(cart)
-        db.commit()
-        db.refresh(cart)
+        await db.commit()
+        await db.refresh(cart)
 
-    exists = (
-        db.query(models.CartItem)
-        .filter(
+    result = await db.execute(
+        select(models.CartItem).filter(
             models.CartItem.cart_id == cart.id, models.CartItem.movie_id == movie_id
         )
-        .first()
     )
+    exists = result.scalars().first()
     if exists:
         raise HTTPException(status_code=400, detail="Movie already in cart.")
 
     cart_item = models.CartItem(cart_id=cart.id, movie_id=movie_id)
     db.add(cart_item)
-    db.commit()
-    db.refresh(cart_item)
+    await db.commit()
+    await db.refresh(cart_item)
     return {"detail": "Movie added to cart", "cart_item_id": cart_item.id}
 
 
 @app.delete("/cart/items/{item_id}", status_code=status.HTTP_200_OK)
-def remove_item_from_cart(
+async def remove_item_from_cart(
     item_id: int,
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_sqlite_db),
     current_user: models.User = Depends(get_current_user),
-) -> models.Item:
-    cart = db.query(models.Cart).filter(models.Cart.user_id == current_user.id).first()
+) -> dict:
+    result = await db.execute(
+        select(models.Cart).filter(models.Cart.user_id == current_user.id)
+    )
+    cart = result.scalars().first()
     if not cart:
         raise HTTPException(status_code=404, detail="Cart not found.")
 
-    cart_item = (
-        db.query(models.CartItem)
-        .filter(models.CartItem.id == item_id, models.CartItem.cart_id == cart.id)
-        .first()
+    result = await db.execute(
+        select(models.CartItem).filter(
+            models.CartItem.id == item_id, models.CartItem.cart_id == cart.id
+        )
     )
+    cart_item = result.scalars().first()
     if not cart_item:
         raise HTTPException(status_code=404, detail="Cart item not found.")
 
-    db.delete(cart_item)
-    db.commit()
+    await db.delete(cart_item)
+    await db.commit()
     return {"detail": "Item removed from cart"}
 
 
 @app.get("/cart", status_code=status.HTTP_200_OK)
-def view_cart(
-    db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)
-) -> models.CartItem:
-    cart = db.query(models.Cart).filter(models.Cart.user_id == current_user.id).first()
+async def view_cart(
+    db: AsyncSession = Depends(get_sqlite_db),
+    current_user: models.User = Depends(get_current_user),
+) -> dict:
+    result = await db.execute(
+        select(models.Cart).filter(models.Cart.user_id == current_user.id)
+    )
+    cart = result.scalars().first()
     if not cart:
         return {"items": []}
 
@@ -114,14 +127,18 @@ def view_cart(
 
 
 @app.delete("/cart", status_code=status.HTTP_200_OK)
-def clear_cart(
-    db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)
-) -> models.CartItem:
-    cart = db.query(models.Cart).filter(models.Cart.user_id == current_user.id).first()
+async def clear_cart(
+    db: AsyncSession = Depends(get_sqlite_db),
+    current_user: models.User = Depends(get_current_user),
+) -> dict:
+    result = await db.execute(
+        select(models.Cart).filter(models.Cart.user_id == current_user.id)
+    )
+    cart = result.scalars().first()
     if not cart:
         raise HTTPException(status_code=404, detail="Cart not found.")
 
     for item in cart.items:
-        db.delete(item)
-    db.commit()
+        await db.delete(item)
+    await db.commit()
     return {"detail": "Cart cleared"}
