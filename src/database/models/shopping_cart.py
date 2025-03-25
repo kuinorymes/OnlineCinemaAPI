223c47b9ba82473e3435ecc583c3e4b440c0c3af
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-25 12:11:44 +0200

    fixed problems with syntax

--- src/database/models/shopping_cart.py
+++ src/database/models/shopping_cart.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
-from database.models.base import Base
+from src.database.models.base import Base
 
 
 class CartItem(Base):
