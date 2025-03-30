20ee1541b9568656948199846899591acdd0ee3f
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-30 21:02:21 +0300

    Fixed endpoints: - View Cart - Clear cart

--- src/routes/cart.py
+++ src/routes/cart.py
@@ -1,6 +1,8 @@
+import traceback
+
 from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, text
 from sqlalchemy.orm import selectinload, joinedload
 
 from database.models.users import UserModel as User
@@ -19,10 +21,20 @@ router = APIRouter()
 async def admin_view_user_cart(
     user_id: int,
     db: AsyncSession = Depends(get_postgres_db),
-    current_admin=Depends(is_admin),
+    current_user_admin=Depends(is_admin),
 ) -> dict:
-    result = await db.execute(select(Cart).filter(Cart.user_id == user_id))
-    cart = result.scalars().first()
+
+    stmt = (
+        select(Cart)
+        .where(Cart.user_id == user_id)
+        .options(
+            selectinload(Cart.items)
+            .selectinload(CartItem.movie)
+        )
+    )
+    result = await db.execute(stmt)
+    cart = result.scalars().all()
+
     if not cart:
         return {"items": []}
 
@@ -134,7 +146,14 @@ async def clear_cart(
     db: AsyncSession = Depends(get_postgres_db),
     current_user: User = Depends(get_current_user),
 ) -> dict:
-    result = await db.execute(select(Cart).filter(Cart.user_id == current_user.id))
+    stmt = (
+        select(Cart)
+        .options(
+            selectinload(Cart.items).selectinload(CartItem.movie)
+        )
+        .where(Cart.user_id == current_user.id)
+    )
+    result = await db.execute(stmt)
     cart = result.scalars().first()
     if not cart:
         raise HTTPException(status_code=404, detail="Cart not found.")
